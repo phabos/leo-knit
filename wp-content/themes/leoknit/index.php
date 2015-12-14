b1b647b6ceacb91a645b1065d6b32d56c7aed6bb
phabos <prezatf@gmail.com>
2015-12-14 23:09:29 +0100

    trust html snippet for description

--- wp-content/themes/leoknit/index.php
+++ wp-content/themes/leoknit/index.php
@@ -30,12 +30,9 @@
           <span class="category category--full">{{ article.tags }}</span>
           <h2 class="title title--full">{{ article.title }}</h2>
           <div class="meta meta--full" ng-style="{'background-image':'url(\'{{article.bigthumb}}\')'}">
-            <!--img class="meta__avatar" src="<?php echo Post::getThumbnail( $post->ID, 'full' )[0] ; ?>" alt="author01" /-->
-            <!--span class="meta__author">Matthew Walters</span-->
             <span class="meta__date"><i class="fa fa-calendar-o"></i> {{ article.date }}</span>
-            <!--span class="meta__reading-time"><i class="fa fa-clock-o"></i> 3 min read</span-->
           </div>
-          <p>{{ article.content }}</p>
+          <p ng-bind-html="article.content"></p>
           <div class="carousel-container">
             <ul rn-carousel class="image" rn-carousel-controls>
               <li ng-repeat="image in article.gallery track by $index">
