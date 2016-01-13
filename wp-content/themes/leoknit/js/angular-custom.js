ed80cd387b2c45c47edadf5ad1eb1d3035b91a95
Fabien PREZAT <f.prezat@jeuneafrique.com>
2016-01-13 12:28:54 +0100

    adding animation for route detail

--- wp-content/themes/leoknit/js/angular-custom.js
+++ wp-content/themes/leoknit/js/angular-custom.js
@@ -53,15 +53,42 @@ app.controller('ArticleCtrl', function($scope, $http, $routeParams, getHttp, mai
     grabMoreArticle();
 });
 /******** ARTICLE DETAIL CONTROLLER ********/
-app.controller('ArticleDetailCtrl', function($scope, $http, $routeParams, $location, getHttp, mainDomain) {
+app.controller('ArticleDetailCtrl', function($scope, $http, $routeParams, $location, getHttp, mainDomain, animateArticle) {
     getHttp.httpRequest(mainDomain.name + '/api/article.json?offset=' + $scope.offset).success(function(data, status, headers, config) {
-        articleId = $routeParams.articleId;
-        console.log(data[articleId].gallery);
-        if (angular.isUndefined(data[articleId])) $location = '/';
-        $scope.article = data[articleId];
+            articleId = $routeParams.articleId;
+            console.log(data[articleId].gallery);
+            if (angular.isUndefined(data[articleId])) $location = '/';
+            $scope.article = data[articleId];
+            animateArticle.showDetail();
+    });
+
+    $scope.$on("$locationChangeStart", function(event) {
+        animateArticle.hideDetail();
     });
 });
-// Factory
+
+/******** Factory ********/
+app.factory('animateArticle', function() {
+    this.showDetail = function() {
+        var bodyEl = jQuery(document.body);
+        bodyEl.addClass('view-single');
+        jQuery('section.content').addClass('content--show');
+        // show content item:
+        jQuery('article.content__item').addClass('content__item--show');
+        // show close control
+        jQuery('section.content .close-button').addClass('close-button--show');
+        // sets overflow hidden to the body and allows the switch to the content scroll
+        bodyEl.addClass('noscroll');
+    };
+    this.hideDetail = function() {
+        var bodyEl = jQuery(document.body);
+        jQuery('article.content__item').removeClass('content__item--show');
+        jQuery('section.content').removeClass('content--show');
+        jQuery('section.content .close-button').removeClass('close-button--show');
+        bodyEl.removeClass('view-single');
+    };
+    return this;
+});
 app.factory('getHttp', function($http) {
     this.httpRequest = function(url) {
         return $http.get(url, {
