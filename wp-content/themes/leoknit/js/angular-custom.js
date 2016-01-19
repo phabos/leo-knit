344c223e1f8b82a1d86a68511125e62d63b655e4
Fabien PREZAT <f.prezat@jeuneafrique.com>
2016-01-19 20:00:14 +0100

    animation works :)

--- wp-content/themes/leoknit/js/angular-custom.js
+++ wp-content/themes/leoknit/js/angular-custom.js
@@ -10,15 +10,12 @@ app.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
         when('/', {
-                templateUrl: '/wp-content/themes/leoknit/partials/article-list.html',
-                controller: 'ArticleCtrl'
-            })
-            /*.
-                    when('/article/:articleId', {
-                        templateUrl: '/wp-content/themes/leoknit/partials/article-detail.html',
-                        controller: 'ArticleDetailCtrl'
-                    })*/
-        ;
+            templateUrl: '/wp-content/themes/leoknit/partials/article-list.html',
+            controller: 'ArticleCtrl'
+        }).when('/contact', {
+            templateUrl: '/wp-content/themes/leoknit/partials/contact-form.html',
+            controller: 'ContactCtrl'
+        });
         /*.
               otherwise({
                 redirectTo: '/phones'
@@ -31,35 +28,34 @@ app.controller('ArticleCtrl', function($scope, $http, $routeParams, getHttp, mai
     $scope.offset = 0;
     $scope.showMore = 0;
     $scope.showNoMore = 0;
-
     // More article button
     $scope.loadMoreArticles = function() {
-        grabMoreArticle();
-    }
-    // Global functio to get the article
+            grabMoreArticle();
+        }
+        // Global functio to get the article
     var grabMoreArticle = function() {
-        $scope.showMore = 0;
-        getHttp.httpRequest(mainDomain.name + '/api/article.json?offset=' + $scope.offset).success(function(data, status, headers, config) {
-            if (data.length > 0) {
-                $scope.showMore = 1;
-            } else {
-                $scope.showMore = 0;
-            }
-            if (data.length == 0) {
-                $scope.showNoMore = 1;
-            }
-            if (angular.isUndefined($scope.articles)) $scope.articles = [];
-            $scope.articles = $scope.articles.concat(data);
-        });
-        $scope.offset++;
-    }
-    // Fire when controller is called
+            $scope.showMore = 0;
+            getHttp.httpRequest(mainDomain.name + '/api/article.json?offset=' + $scope.offset).success(function(data, status, headers, config) {
+                if (data.length > 0) {
+                    $scope.showMore = 1;
+                } else {
+                    $scope.showMore = 0;
+                }
+                if (data.length == 0) {
+                    $scope.showNoMore = 1;
+                }
+                if (angular.isUndefined($scope.articles)) $scope.articles = [];
+                $scope.articles = $scope.articles.concat(data);
+            });
+            $scope.offset++;
+        }
+        // Fire when controller is called
     grabMoreArticle();
-    $scope.loadDetailArticle = function(elt) {
-        animateArticle.beforeShowDetail(elt);
+    $scope.loadDetailArticle = function(elt, pos) {
+        animateArticle.beforeLoadContent(elt, pos);
     }
     $scope.hideContent = function() {
-        animateArticle.hideDetail();
+        animateArticle.hideContent();
     }
     /*$scope.$on("$locationChangeStart", function(event) {
         var elt = jQuery('#grid_item_' + event.targetScope.$id);
@@ -80,98 +76,149 @@ app.controller('ArticleCtrl', function($scope, $http, $routeParams, getHttp, mai
         animateArticle.hideDetail();
     });
 });*/
+app.controller('ContactCtrl', function($scope, $http, $routeParams, $location, getHttp, mainDomain, animateArticle) {
+    animateArticle.showDetail();
+    $scope.$on("$locationChangeStart", function(event) {
+        animateArticle.hideDetail();
+    });
+});
 /******** Factory ********/
+app.value('current', -1);
 app.value('lockScroll', false);
 app.value('xscroll', '');
 app.value('yscroll', '');
 app.value('isAnimating', false);
-app.factory('animateArticle', function(lockScroll, xscroll, yscroll, isAnimating) {
-    this.showDetail = function() {
-        var bodyEl = jQuery(document.body);
-        bodyEl.addClass('view-single');
-        jQuery('section.content').addClass('content--show');
-        // show content item:
-        jQuery('article.content__item').addClass('content__item--show');
-        // show close control
-        jQuery('section.content .close-button').addClass('close-button--show');
-        // sets overflow hidden to the body and allows the switch to the content scroll
-        bodyEl.addClass('noscroll');
-    };
-    this.hideDetail = function() {
-        console.log('close');
-        /*var gridItem = gridItems[current], contentItem = contentItems[current];
-
-        classie.remove(contentItem, 'content__item--show');
-        classie.remove(contentItemsContainer, 'content--show');
-        classie.remove(closeCtrl, 'close-button--show');
-        classie.remove(bodyEl, 'view-single');
-
-        setTimeout(function() {
-            var dummy = gridItemsContainer.querySelector('.placeholder');
-
-            classie.removeClass(bodyEl, 'noscroll');
-
-            dummy.style.WebkitTransform = 'translate3d(' + gridItem.offsetLeft + 'px, ' + gridItem.offsetTop + 'px, 0px) scale3d(' + gridItem.offsetWidth/gridItemsContainer.offsetWidth + ',' + gridItem.offsetHeight/getViewport('y') + ',1)';
-            dummy.style.transform = 'translate3d(' + gridItem.offsetLeft + 'px, ' + gridItem.offsetTop + 'px, 0px) scale3d(' + gridItem.offsetWidth/gridItemsContainer.offsetWidth + ',' + gridItem.offsetHeight/getViewport('y') + ',1)';
-
-            onEndTransition(dummy, function() {
-                // reset content scroll..
-                contentItem.parentNode.scrollTop = 0;
-                gridItemsContainer.removeChild(dummy);
-                classie.remove(gridItem, 'grid__item--loading');
-                classie.remove(gridItem, 'grid__item--animate');
-                lockScroll = false;
-                window.removeEventListener( 'scroll', noscroll );
-            });
-
-            // reset current
-            current = -1;
-        }, 25);*/
-    };
-    this.beforeShowDetail = function(elt) {
-        jQuery('#' + elt).addClass('grid__item--loading');
+app.value('item', '');
+app.value('article', '');
+app.factory('animateArticle', function(current, lockScroll, xscroll, yscroll, isAnimating) {
+    this.beforeLoadContent = function(elt, pos) {
+        console.log(elt + ' ' + pos);
         var obj = this;
+        item = document.getElementById('grid_item_' + elt);
+        article = document.getElementById('grid_article_' + elt);
+
+        if(isAnimating || current === pos) {
+            return false;
+        }
+        isAnimating = true;
+        // index of current item
+        current = pos;
+        // simulate loading time..
+        item.classList.add('grid__item--loading');
+
         setTimeout(function() {
-            jQuery('#' + elt).addClass('grid__item--animate');
-            setTimeout(function() { obj.loadContent(elt); }, 500);
+            item.classList.add('grid__item--animate');
+            // reveal/load content after the last element animates out (todo: wait for the last transition to finish)
+            setTimeout(function() { obj.loadContent(item); }, 500);
         }, 1000);
     };
-    this.loadContent = function(elt) {
-        var item = jQuery('#' + elt);
-        var gridEl = document.getElementById('theGrid');
-        var gridItemsContainer = gridEl.querySelector('section.grid');
-        var bodyEl = document.body;
-        var obj = this;
+    this.loadContent = function() {
+        console.log('loading content');
+        var gridEl = document.getElementById('theGrid'),
+            gridItemsContainer = gridEl.querySelector('section.grid'),
+            contentItemsContainer = gridEl.querySelector('section.content'),
+            contentItems = contentItemsContainer.querySelectorAll('.content__item'),
+            closeCtrl = contentItemsContainer.querySelector('.close-button'),
+            bodyEl = document.body,
+            obj= this;
         // add expanding element/placeholder
         var dummy = document.createElement('div');
         dummy.className = 'placeholder';
 
         // set the width/heigth and position
-        jQuery(dummy).css('WebkitTransform', 'translate3d(' + (item.offsetLeft - 5) + 'px, ' + (item.offsetTop - 5) + 'px, 0px) scale3d(' + item.offsetWidth/gridItemsContainer.offsetWidth + ',' + item.offsetHeight/this.getViewport('y') + ',1)');
-        jQuery(dummy).css('transform', 'translate3d(' + (item.offsetLeft - 5) + 'px, ' + (item.offsetTop - 5) + 'px, 0px) scale3d(' + item.offsetWidth/gridItemsContainer.offsetWidth + ',' + item.offsetHeight/this.getViewport('y') + ',1)');
+        dummy.style.WebkitTransform = 'translate3d(' + (item.offsetLeft - 5) + 'px, ' + (item.offsetTop - 5) + 'px, 0px) scale3d(' + item.offsetWidth/gridItemsContainer.offsetWidth + ',' + item.offsetHeight/this.getViewport('y') + ',1)';
+        dummy.style.transform = 'translate3d(' + (item.offsetLeft - 5) + 'px, ' + (item.offsetTop - 5) + 'px, 0px) scale3d(' + item.offsetWidth/gridItemsContainer.offsetWidth + ',' + item.offsetHeight/this.getViewport('y') + ',1)';
 
         // add transition class
-        jQuery(dummy).addClass('placeholder--trans-in');
+        dummy.classList.add('placeholder--trans-in');
 
         // insert it after all the grid items
         gridItemsContainer.appendChild(dummy);
 
         // body overlay
-        jQuery(bodyEl).addClass('view-single');
+        bodyEl.classList.add('view-single');
 
         setTimeout(function() {
             // expands the placeholder
-            jQuery(dummy).css('WebkitTransform', 'translate3d(-5px, ' + (obj.scrollY() - 5) + 'px, 0px)');
-            jQuery(dummy).css('transform', 'translate3d(-5px, ' + (obj.scrollY() - 5) + 'px, 0px)');
+            dummy.style.WebkitTransform = 'translate3d(-5px, ' + (obj.scrollY() - 5) + 'px, 0px)';
+            dummy.style.transform = 'translate3d(-5px, ' + (obj.scrollY() - 5) + 'px, 0px)';
             // disallow scroll
             window.addEventListener('scroll', obj.noscroll(obj));
         }, 25);
 
-        jQuery(dummy).removeClass('placeholder--trans-in');
-        jQuery(dummy).addClass('placeholder--trans-out');
-        // position the content container
-        jQuery('section.content').css('top', this.scrollY());
-        this.showDetail();
+        this.onEndTransition(dummy, function() {
+            // add transition class
+            dummy.classList.remove('placeholder--trans-in');
+            dummy.classList.add('placeholder--trans-out');
+            // position the content container
+            contentItemsContainer.style.top = obj.scrollY() + 'px';
+            // show the main content container
+            contentItemsContainer.classList.add('content--show');
+            // show content item:
+            article.classList.add('content__item--show');
+            // show close control
+            closeCtrl.classList.add('close-button--show');
+            // sets overflow hidden to the body and allows the switch to the content scroll
+            bodyEl.classList.add('noscroll');
+
+            isAnimating = false;
+        });
+    };
+    this.hideContent = function() {
+        console.log('hidding content');
+        var gridItem = item,
+            contentItem = article,
+            gridEl = document.getElementById('theGrid'),
+            contentItemsContainer = gridEl.querySelector('section.content'),
+            closeCtrl = contentItemsContainer.querySelector('.close-button'),
+            bodyEl = document.body,
+            gridItemsContainer = gridEl.querySelector('section.grid'),
+            obj = this;
+
+        contentItem.classList.remove('content__item--show');
+        contentItemsContainer.classList.remove('content--show');
+        closeCtrl.classList.remove('close-button--show');
+        bodyEl.classList.remove('view-single');
+
+        setTimeout(function() {
+            var dummy = gridItemsContainer.querySelector('.placeholder');
+
+            bodyEl.classList.remove('noscroll');
+
+            dummy.style.WebkitTransform = 'translate3d(' + gridItem.offsetLeft + 'px, ' + gridItem.offsetTop + 'px, 0px) scale3d(' + gridItem.offsetWidth/gridItemsContainer.offsetWidth + ',' + gridItem.offsetHeight/obj.getViewport('y') + ',1)';
+            dummy.style.transform = 'translate3d(' + gridItem.offsetLeft + 'px, ' + gridItem.offsetTop + 'px, 0px) scale3d(' + gridItem.offsetWidth/gridItemsContainer.offsetWidth + ',' + gridItem.offsetHeight/obj.getViewport('y') + ',1)';
+
+            obj.onEndTransition(dummy, function() {
+                // reset content scroll..
+                contentItem.parentNode.scrollTop = 0;
+                gridItemsContainer.removeChild(dummy);
+                gridItem.classList.remove('grid__item--loading');
+                gridItem.classList.remove('grid__item--animate');
+                lockScroll = false;
+                window.removeEventListener( 'scroll', obj.noscroll );
+            });
+
+            // reset current
+            current = -1;
+        }, 25);
+    };
+    this.onEndTransition = function( el, callback ) {
+        var support = { transitions: Modernizr.csstransitions },
+            transEndEventNames = { 'WebkitTransition': 'webkitTransitionEnd', 'MozTransition': 'transitionend', 'OTransition': 'oTransitionEnd', 'msTransition': 'MSTransitionEnd', 'transition': 'transitionend' },
+            transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ];
+        var onEndCallbackFn = function( ev ) {
+            if( support.transitions ) {
+                if( ev.target != this ) return;
+                this.removeEventListener( transEndEventName, onEndCallbackFn );
+            }
+            if( callback && typeof callback === 'function' ) { callback.call(this); }
+        };
+        if( support.transitions ) {
+            el.addEventListener( transEndEventName, onEndCallbackFn );
+        }
+        else {
+            onEndCallbackFn();
+        }
     };
     this.getViewport = function( axis ) {
         var docElem = window.document.documentElement;
@@ -187,12 +234,12 @@ app.factory('animateArticle', function(lockScroll, xscroll, yscroll, isAnimating
 
         return client < inner ? inner : client;
     };
-    this.scrollX = function() {
-        var docElem = window.document.documentElement;
+    this.scrollX = function () {
+        var docElem = window.document.documentElement
         return window.pageXOffset || docElem.scrollLeft;
     };
     this.scrollY = function() {
-        var docElem = window.document.documentElement;
+        var docElem = window.document.documentElement
         return window.pageYOffset || docElem.scrollTop;
     };
     this.noscroll = function(obj) {
@@ -202,37 +249,7 @@ app.factory('animateArticle', function(lockScroll, xscroll, yscroll, isAnimating
             yscroll = obj.scrollY();
         }
         window.scrollTo(xscroll, yscroll);
-    };
-    /*function hideContent() {
-        var gridItem = gridItems[current], contentItem = contentItems[current];
-
-        classie.remove(contentItem, 'content__item--show');
-        classie.remove(contentItemsContainer, 'content--show');
-        classie.remove(closeCtrl, 'close-button--show');
-        classie.remove(bodyEl, 'view-single');
-
-        setTimeout(function() {
-            var dummy = gridItemsContainer.querySelector('.placeholder');
-
-            classie.removeClass(bodyEl, 'noscroll');
-
-            dummy.style.WebkitTransform = 'translate3d(' + gridItem.offsetLeft + 'px, ' + gridItem.offsetTop + 'px, 0px) scale3d(' + gridItem.offsetWidth/gridItemsContainer.offsetWidth + ',' + gridItem.offsetHeight/getViewport('y') + ',1)';
-            dummy.style.transform = 'translate3d(' + gridItem.offsetLeft + 'px, ' + gridItem.offsetTop + 'px, 0px) scale3d(' + gridItem.offsetWidth/gridItemsContainer.offsetWidth + ',' + gridItem.offsetHeight/getViewport('y') + ',1)';
-
-            onEndTransition(dummy, function() {
-                // reset content scroll..
-                contentItem.parentNode.scrollTop = 0;
-                gridItemsContainer.removeChild(dummy);
-                classie.remove(gridItem, 'grid__item--loading');
-                classie.remove(gridItem, 'grid__item--animate');
-                lockScroll = false;
-                window.removeEventListener( 'scroll', noscroll );
-            });
-
-            // reset current
-            current = -1;
-        }, 25);
-    }*/
+    }
     return this;
 });
 app.factory('getHttp', function($http) {
